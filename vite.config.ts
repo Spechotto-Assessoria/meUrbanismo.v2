2ce4380c9e1fc1b3fe880d70106904bef5db4a38
Rennan Spechotto <rennan.spechotto@gmail.com>
2026-08-28 07:35:43 -0400

    fix: aplica configuracoes de rotas e força rebuild com variaveis da vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    host: true
+  base: '/',
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
   }
 });
