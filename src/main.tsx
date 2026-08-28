b40cffca8da567a28f5104819d44d9cf42acb240
Rennan Spechotto <rennan.spechotto@gmail.com>
2026-08-28 09:30:31 -0400

    fix: corrige montagem do DOM em main.tsx e garante renderizacao sem tela branca

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { AuthProvider } from './contexts/AuthContext';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </React.StrictMode>
+  );
+} else {
+  console.error('Elemento root nao encontrado no index.html');
+}
